96bde93317d2db7b3259941ad3eff30ec273fa74
Martins Zeltins <queeeeenz@gmail.com>
2026-08-28 18:45:25 +0300

    js fs demo

--- res/fs-table/fs-table.js
+++ res/fs-table/fs-table.js
@@ -8,7 +8,7 @@ const fileSystemTable = [{
         type: 'dir',
         start: 0,
         size: 0,
-        parentId: null,
+        parentDirId: null,
     },
 
     {
@@ -17,21 +17,22 @@ const fileSystemTable = [{
         type: 'dir',
         start: 0,
         size: 0,
-        parentId: 1,
+        parentDirId: 1,
     },
 
     {
         id: 3,
         name: 'kernel',
         type: 'file',
-        start: 0,
-        size: 100,
-        parentId: 2,
+        start: 14562,
+        size: 5489,
+        parentDirId: 2,
     },
 ]
 
 /**
  * Simple demonstration of how file lookup works
+ * You can ignore this.
  */
 const simpleOpen = (path) => {
     /**
@@ -40,8 +41,8 @@ const simpleOpen = (path) => {
     const parts = path.split('/')
 
     const systemDir = fileSystemTable.find(fsEntry => fsEntry.name === parts[1])
-    const bootDir = fileSystemTable.find(fsEntry => fsEntry.name === parts[2] && fsEntry.parentId === systemDir.id)
-    const kernelFile = fileSystemTable.find(fsEntry => fsEntry.name === parts[3] && fsEntry.parentId === bootDir.id)
+    const bootDir = fileSystemTable.find(fsEntry => fsEntry.name === parts[2] && fsEntry.parentDirId === systemDir.id)
+    const kernelFile = fileSystemTable.find(fsEntry => fsEntry.name === parts[3] && fsEntry.parentDirId === bootDir.id)
 
     return kernelFile
 }
@@ -54,30 +55,30 @@ const simpleOpen = (path) => {
  * by first finding the root (parentDirId = null). Once we find it, we update
  * the parentDirId with the found entry's id and continue to the next part. Now
  * parentDirId is no longer null but e.g. 1 and we try to find the entry with
- * name and parentId. Every time we find it we update our foundEntry and parentDirId.
+ * name and parentDirId. Every time we find it we update our foundEntry and parentDirId.
  * 
  * At the end we have looped over each file path part and foundEntry is the last
  * found entry which is the actual file we were looking for.
  * 
  * Example: /system/boot/kernel
  *
- * parentId = null
+ * parentDirId = null
 
  * find:
  * name = system
- * parentId = null
+ * parentDirId = null
  * ↓
  * found id = 1
 
  * find:
  * name = boot
- * parentId = 1
+ * parentDirId = 1
  * ↓
  * found id = 2
 
  * find:
  * name = kernel
- * parentId = 2
+ * parentDirId = 2
  * ↓
  * found file
  */
@@ -88,9 +89,9 @@ const open = (path) => {
     let foundEntry
     let parentDirId = null
 
-    for (i = 0; i < pathParts.length; i++) {
+    for (let i = 0; i < pathParts.length; i++) {
         foundEntry = fileSystemTable.find(fsEntry => {
-            return fsEntry.name === pathParts[i] && fsEntry.parentId === parentDirId
+            return fsEntry.name === pathParts[i] && fsEntry.parentDirId === parentDirId
         })
 
         parentDirId = foundEntry.id
