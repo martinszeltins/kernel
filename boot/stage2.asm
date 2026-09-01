eca07690cc8c2273b369b8ce1ea49ddfe9ab8536
Martins Zeltins <queeeeenz@gmail.com>
2026-09-01 08:10:34 +0300

    Stage 2 wip

--- boot/stage2.asm
+++ boot/stage2.asm
@@ -1,8 +1,10 @@
-mov ax, 0xB800              ; VGA text memory segment
+org 0x7E00                    ; Stage 2 loads us in RAM at 7E00 (31.5 KB)
+
+mov ax, 0xB800                ; VGA text memory segment
 mov es, ax
 
-mov byte [es:0], '2'        ; show "2" on screen
+mov byte [es:0], '2'          ; show "2" on screen
 
-jmp $                       ; stay here forever
+jmp $                         ; stay here forever
 
-times 512 - ($ - $$) db 0   ; make Stage 2 exactly 512 bytes
+times 65024 - ($ - $$) db 0   ; make Stage 2 exactly 65024 bytes (63.5 KB)
