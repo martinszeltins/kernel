6e8504ec676b476c94b17342aa5f118ff9a7b018
Martins Zeltins <queeeeenz@gmail.com>
2026-09-01 19:55:25 +0300

    [master] stage2: entering protected mode

--- boot/stage2.asm
+++ boot/stage2.asm
@@ -1,10 +1,38 @@
-org 0x7E00                    ; Stage 2 loads us in RAM at 7E00 (31.5 KB)
+org 0x7E00                             ; Stage 2 loads us in RAM at 7E00 (31.5 KB)
 
-mov ax, 0xB800                ; VGA text memory segment
-mov es, ax
+cli                                    ; disable interrupts
+lgdt [gdt_descriptor]                  ; load gdt
 
-mov byte [es:0], '2'          ; show "2" on screen
+                                       ; turn on protected mode
+mov eax, cr0                           ; copy the current CR0 value into EAX
+or eax, 0x00000001                     ; turn on bit 0 (CR0.PE), leave all other bits unchanged
+mov cr0, eax                           ; copy the modified value back into CR0
 
-jmp $                         ; stay here forever
+jmp 0x08:protected_mode                ; set CS = 0x08
 
-times 65024 - ($ - $$) db 0   ; make Stage 2 exactly 65024 bytes (63.5 KB)
+[bits 32]
+protected_mode:
+    jmp $                              ; stay here forever for now
+
+gdt:
+    db 0, 0, 0, 0, 0, 0, 0, 0          ; entry #0 - null
+
+                                       ; entry #1 - code segment (FF FF 00 00 00 9A CF 00)
+    db 0xff, 0xff                      ; limit: 4 GB
+    db 0, 0, 0                         ; base: 0
+    db 10011010b                       ; permissions: code, readable, ring 0, present
+    db 11001111b                       ; 32bit, 4 KB granularity, upper limit = F
+    db 0                               ; base: 0
+
+                                       ; entry #2 - data segment (FF FF 00 00 00 92 CF 00)
+    db 0xff, 0xff                      ; limit: 4 GB
+    db 0, 0, 0                         ; base: 0
+    db 10010010b                       ; permissions: data, writable, ring 0, present
+    db 11001111b                       ; 32bit, 4 KB granularity, upper limit = F
+    db 0                               ; base: 0
+
+gdt_descriptor:
+    dw 23                              ; gdt size
+    dd gdt                             ; gdt address
+
+times 65024 - ($ - $$) db 0            ; make Stage 2 exactly 65024 bytes (63.5 KB)
