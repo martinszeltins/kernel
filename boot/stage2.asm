9cc4791fe3c8bcb784bc72cee14a75dca029121b
Martins Zeltins <queeeeenz@gmail.com>
2026-09-04 19:24:43 +0300

    setup paging, create pml4

--- boot/stage2.asm
+++ boot/stage2.asm
@@ -12,9 +12,33 @@ jmp 0x08:protected_mode                ; set CS = 0x08
 
 [bits 32]
 protected_mode:
-    mov ax, 0x10
-    mov ds, ax
-    mov byte [0xB8000], 'H'
+    mov ax, 0x10                       ; allows us to access memory in 32-bit protected mode,
+    mov ds, ax                         ; set data segment to 0x10 (gdt selector for 32-bit mode)
+    mov ss, ax                         ; set stack segment to 0x10 (gdt selector for 32-bit mode)
+    mov es, ax                         ; set extra segment to 0x10 (gdt selector for 32-bit mode)
+    mov fs, ax                         ; set fs segment to 0x10 (gdt selector for 32-bit mode)
+    mov gs, ax                         ; set gs segment to 0x10 (gdt selector for 32-bit mode)
+    mov esp, 0x7C00                    ; set the stack pointer to 31 KB
+
+                                       ; Set up Paging
+                                       ; Create Level 4 (PML4) which will live at 96 KB (right after stage 2)
+                                       ; It contains 512 entries. Each entry is 8 bytes (64 bits) (4 KB in total)
+
+                                       ; Create PML4 Table  (96 KB - 100 KB)
+    mov eax, 98304                     ; 96 KB - address of first PML4 entry
+    mov ebx, 512                       ; number of entries to clear
+
+    create_pml4:
+        mov dword [eax], 0             ; clear first 32 bits of this entry
+        mov dword [eax + 4], 0         ; clear second 32 bits of this entry
+
+        add eax, 8                     ; point EAX at the next 8-byte entry
+
+        sub ebx, 1                     ; one fewer entry left
+        cmp ebx, 0                     ; are we finished?
+        jne create_pml4                ; no → create the next entry
+
+    mov byte [0xB8000], 'H'            ; just put H on the screen
     
     jmp $                              ; stay here forever for now
 
---
+++ res/paging.md
@@ -0,0 +1,9 @@
+Level 4 (PML4) = Page Map Level 4
+Level 3 (PDPT) = Page Directory Pointer Table
+Level 2 (PD)   = Page Directory
+Level 1 (PT)   = Page Table
+
+Level 4 (PML4)
+
+|| PML4E || PML4E || PML4E || PML4E || PML4E || PML4E || PML4E || PML4E || PML4E || PML4E || PML4E || PML4E || PML4E ||
+|| flag 1 | flag 2 | | PDPT pointer | flat 3 | flag 4 || flag 1 | flag 2 | | PDPT pointer | flat 3 | flag 4 ||
